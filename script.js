f45356638f9d63473d85cd8fbda6b172ab3e3499
Paul Marechal <paulmarechal785@hotmail.fr>
2024-10-15 11:20:53 +0200

    Update AR Buttons + CSS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const scanCardTitle = document.getElementById('scan_card_title');
 const infoContainer = document.getElementById('info-container');
 const target0 = document.getElementById('target-0')
 const target1 = document.getElementById('target-1')
+const contact_by_mail_ar = document.getElementById('contact_by_mail_ar')
 
 function hideScanMessage() {
     scanCardTitle.style.display = 'none';
@@ -25,3 +26,49 @@ target1.addEventListener('targetFound', () => {
 });
 
 target1.addEventListener('targetLost', resetUI);
+
+
+AFRAME.registerComponent('touch-raycaster', {
+    init() {
+        const scene = this.el.sceneEl;
+        const camera = this.el.sceneEl.camera;
+
+        document.querySelector("#touch-overlay").addEventListener('click', (event) => {
+            const mouse = new THREE.Vector2();
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+            const raycaster = new THREE.Raycaster();
+            raycaster.setFromCamera(mouse, camera);
+
+            const intersects = raycaster.intersectObjects(scene.object3D.children, true);
+
+            if (intersects.length > 0) {
+                for (const intersect of intersects) {
+                    const el = intersect.object.el;
+                    if (el) {
+                        const name = el.getAttribute('name'); 
+
+                        switch (name) {
+                            case 'email':
+                                window.location.href = 'mailto:contactme@paulmarechal.xyz';
+                                break;
+                            case 'website':
+                                window.location.href = 'https://devxr.fr';
+                                break;
+                            case 'telephone':
+                                window.location.href = 'tel:+33648357374';
+                                break;
+                            default:
+                                console.log('No action defined for this element');
+                        }
+                    }
+                }
+
+                
+            } else {
+                console.log('No intersections found.');
+            }
+        });
+    }
+});
